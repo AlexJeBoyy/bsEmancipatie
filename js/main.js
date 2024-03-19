0385821ae9189a9d5e7247cf7373002e8e889258
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-03-19 14:10:58 +0100

    blep

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-window.onload = (event) => {
 
+function LoadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", page, true);
+    xhttp.send();
 }
